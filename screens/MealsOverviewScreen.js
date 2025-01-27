f7d965912062521139050733b56586a5f993d9ea
Vishal Shinde <vishalshinde541@gmail.com>
2025-01-27 18:07:59 +0530

    updated code to display meal titel as per selected category

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,29 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+import MealItem from "../components/MealItem";
 
 // import { useRoute } from "@react-navigation/native";
 
 function MealsOverviewScreen({ route }) {
   //   const route = useRoute();
   const catId = route.params.categoryId;
+
+  const mealsToDisplay = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.includes(catId.toString());
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
+      <FlatList
+        data={mealsToDisplay}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
       <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
