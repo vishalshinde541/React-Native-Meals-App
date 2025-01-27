b9f9b50aa989e40738369f198db77e51f14d57cd
Vishal Shinde <vishalshinde541@gmail.com>
2025-01-27 17:23:09 +0530

    passed category Id through navigation using params and route

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet } from "react-native";
 
-function MealsOverviewScreen() {
+// import { useRoute } from "@react-navigation/native";
+
+function MealsOverviewScreen({ route }) {
+  //   const route = useRoute();
+  const catId = route.params.categoryId;
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 }
