58fd6e7913402a35f72b00d2427306e804ee1142
Vishal Shinde <vishalshinde541@gmail.com>
2025-01-28 12:18:39 +0530

    used usrLayoutEfect hook and navigation.setOptions method to set the dynamic title to MealOverview screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,17 +1,28 @@
+import { useLayoutEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 import MealItem from "../components/MealItem";
 
 // import { useRoute } from "@react-navigation/native";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   //   const route = useRoute();
   const catId = route.params.categoryId;
 
   const mealsToDisplay = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.includes(catId.toString());
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
